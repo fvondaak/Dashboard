2ba5c284bc4da9accc733355c189a0c91a46697d
@00000000014BA0A2 <fabio.daak@tum.de>
2026-07-26 23:01:01 +0200

    feat: added PacketParser with basic functionality

--- src/serial_worker.py
+++ src/serial_worker.py
@@ -49,3 +49,41 @@ class PortReader():
     def is_open(self) -> bool:
         '''returns True if port is open'''
         return self.serial_instance.is_open
+
+class PacketParser:
+    def __init__(self, start_sequence: bytearray, sample_size: int, packet_length: int, max_samples: int):
+        self._buffer = bytearray()
+        self._start = start_sequence
+        self._sample_size = sample_size
+        self._packet_length = packet_length
+        self._max_samples = max_samples
+
+    def feed(self, chunk: bytes):
+        '''function that appends chunck to bytearray'''
+        self._buffer += bytearray(chunk)
+
+    def search_for_start(self, start: bytearray, byte_array: bytearray) -> int:
+        ''' returns index of the start sequence inside the bytearray'''
+        return byte_array.find(start)
+
+    def verify_byte_array(self, packet: bytearray, max_samples):
+        ''' function that verifies that start sequence is present 
+            and sample number is inseide value range'''#
+        if len(packet) != self._packet_length:
+            return False
+        if 0 < int.from_bytes(
+            packet[2:4],
+            byteorder="little",
+            signed=False
+            ) < max_samples and packet[0:2] == self._start:
+            return True
+        else:
+            return False
+        
+    def extract_packet(self, array: bytearray, start_index: int, packet_length: int, max_samples):
+        if self.verify_byte_array(array[start_index:start_index+packet_length], max_samples=self._max_samples) is True:
+            packet = bytes(self._buffer[:self._packet_length])
+            del self._buffer[:self._packet_length]
+            return packet
+        
+    
